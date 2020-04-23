cad82a03c2b37e9ab80534bc7b8957ee64b44aa8
Sheep Geminis <60444423+SheepGeminis@users.noreply.github.com>
2020-04-23 18:19:43 -0500

    Añadidos archivos .html, .css y .js del carrusel
    El carrusel es completamente funcional

---
+++ carrousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#carrousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">PREV</button>',
+    nextArrow: '<button class="slick-next" aria-label="Next" type="button">NEXT</button>',
+  });
+});
+
