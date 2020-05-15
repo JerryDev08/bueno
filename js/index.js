603222465d79f5189c84ea353257a2f2f12ffde4
Gerardo Hernandez <gerh1008@gmail.com>
2020-05-15 05:15:33 -0500

    Se modifico TODO xD

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-    $('#carrousel').slick({
+$(document).ready(function () {
+  $("#carrousel").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
-    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">PREV</button>',
-    nextArrow: '<button class="slick-next" aria-label="Next" type="button">NEXT</button>',
+    prevArrow:
+      '<button class="slick-prev" aria-label="Previous" type="button">PREV</button>',
+    nextArrow:
+      '<button class="slick-next" aria-label="Next" type="button">NEXT</button>',
   });
 });
-
